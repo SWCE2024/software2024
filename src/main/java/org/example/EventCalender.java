002da2aa5ad017dc24a5b822dced61b87075e09a
shahd <shadthabit@gmail.com>
2024-03-22 03:09:34 +0200

    Merge remote-tracking branch 'origin/main'

--- src/main/java/org/example/EventCalender.java
+++ src/main/java/org/example/EventCalender.java
@@ -4,7 +4,6 @@ import javafx.beans.property.SimpleStringProperty;
 
 public class EventCalender {
 
-
     private final SimpleStringProperty eventName;
     private final SimpleStringProperty eventDate;
     private final SimpleStringProperty eventTime;
