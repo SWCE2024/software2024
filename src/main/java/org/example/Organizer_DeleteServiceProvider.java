e4d3d164323fa44371f1d98a1bc6e0974f1618d5
noorata <nooratta17@gmail.com>
2024-03-23 23:25:57 +0300

    99

--- src/main/java/org/example/Organizer_DeleteServiceProvider.java
+++ src/main/java/org/example/Organizer_DeleteServiceProvider.java
@@ -32,7 +32,7 @@ public class Organizer_DeleteServiceProvider {
     void backClicked(MouseEvent event) {
         try {
             Parent root;
-            root = FXMLLoader.load(getClass().getResource("/org.example/OrganizerVendorManegement.fxml"));
+            root = FXMLLoader.load(getClass().getResource("/org.example/OrganizerVendorManagement.fxml"));
             Stage stage=(Stage) back.getScene().getWindow();
             stage.setScene(new Scene(root));
             stage.show();
